1ff0ac2744f690097fc84767a87d242e9c3732f6
nollive <olivier.gaufres@gmail.com>
2024-06-17 17:13:06 +0200

    added script to generate a gif with the localization of individual over time (launched on IP cluster)

--- R/nodscov2/dev-test-gif.R
+++ R/nodscov2/dev-test-gif.R
@@ -5,24 +5,27 @@ library(dplyr)
 library(ggrepel)
 library(tidyr)
 
-# Function to distribute points in a circle
+loc_path <- file.path("C:/Users/Olivier/Documents/CNAM-PASTEUR-FIXE/projet-pacri/out/loc-nodscov2/dev-localization-nodscov2.RData")
+load(loc_path) ##overwrite admission
+
 distribute_points <- function(n) {
   theta <- seq(0, 2 * pi, length.out = n + 1)[-1]
-  radius <- 0.3  # Adjust as needed
+  radius <- 0.2  # Radius of the circle where the points will be plotted 
   x <- radius * cos(theta)
   y <- radius * sin(theta)
   return(data.frame(offset_x = x, offset_y = y))
 }
 
-# Prepare data
-chambres <- rooms %>%
+
+rooms_coords <- rooms %>%
   mutate(
     x = case_when(
       grepl("001", id) ~ id_room,
       grepl("Corridor", room) ~ 1,
       room == "Office" ~ 1,
-      room == "Nursing station" ~ 1,
-      grepl("Restroom", room) ~ (id_room %% 2) + 1,
+      room == "Nursing station" ~ 3,
+      room == "Medical Restroom"~ 1,
+      room == "Paramedical Restroom"~ 3,
       TRUE ~ NA_real_
     ),
     y = case_when(
@@ -38,40 +41,58 @@ chambres <- rooms %>%
   distinct(id_room, .keep_all = TRUE) %>%
   select(localization, x, y)
 
-all_data <- do.call(rbind, global_localization) %>% filter(time < 150)
-all_data <- all_data %>%
+data <- do.call(rbind, global_localization) %>% filter(time < 150)
+data <- data %>%
   filter(localization != -1) %>%
-  left_join(chambres, by = c("localization")) %>%
+  left_join(rooms_coords, by = c("localization")) %>%
   left_join(admission, by = c("id")) %>%
   mutate(cat = ifelse(is.na(cat), "patient", cat)) %>%
   filter(cat != "patient")
 
 
 
-# Determine the maximum number of individuals in the same localization at the same time
-max_n <- all_data %>%
+max_n <- data %>%
   group_by(localization, time) %>%
   summarise(n = n(), .groups = 'drop') %>%
   summarise(max_n = max(n)) %>%
   pull(max_n)
 
-test <- all_data %>%
+
+
+apply_offsets <- function(group_by_data) {
+  n <- nrow(group_by_data)
+  offsets <- distribute_points(n)
+  group_by_data <- group_by_data %>%
+    mutate(offset_x = offsets$offset_x, offset_y = offsets$offset_y)
+  return(group_by_data)
+}
+
+
+data <- data %>%
   group_by(localization, time) %>%
-  mutate(count = n()) %>%
-  mutate(offset = list(distribute_points(count))) %>%
-  unnest(cols = offset) %>%
-  mutate(x_adj = x + offset_x, 
-         y_adj = y + offset_y) %>%
+  group_modify(~ apply_offsets(.x)) %>%
+  ungroup() %>%
+  mutate(x_adj = x + offset_x, y_adj = y + offset_y)
+
+
+
+segments_data <- data %>%
+  group_by(id) %>%
+  arrange(time) %>%
+  mutate(x_end = lead(x_adj),
+         y_end = lead(y_adj)) %>%
+  filter(!is.na(x_end) & !is.na(y_end)) %>%
   ungroup()
 
+
 # Plot
-p <- ggplot(all_data, aes(x = x_adj, y = y_adj, color = cat, group = id)) +
+p <- ggplot(data, aes(x = x_adj, y = y_adj, color = cat, group = id)) +
   geom_point(size = 5) +
-  geom_path(aes(group = id), alpha = 1) +
+  #geom_segment(data = segments_data, aes(xend = x_end, yend = y_end), alpha = 0.5) +
   geom_text_repel(aes(label = id), size = 3, box.padding = 0.5, point.padding = 0.5, max.overlaps = Inf) +
   scale_color_discrete(name = "Category") +
   scale_y_continuous(breaks = 1:4, labels = c("Chambre", "Corridor", "Office / Nursing station", "Restroom")) +
-  scale_x_continuous(breaks = 1:sum(chambres$y == 1), labels = c(paste0("Chambre ", 1:sum(chambres$y == 1)))) +
+  scale_x_continuous(breaks = 1:sum(rooms_coords$y == 1), labels = c(paste0("Chambre ", 1:sum(rooms_coords$y == 1)))) +
   labs(title = 'Déplacement des individus au temps: {frame_time}', x = 'X', y = 'Type de pièce') +
   theme_minimal()
 
@@ -79,4 +100,8 @@ animation <- p +
   transition_time(time) +
   ease_aes('linear')
 
-animate(animation, renderer = gifski_renderer("deplacement.gif"), width = 2000, height = 1333, duration = 20)
+
+nframes <- nrow(data%>%distinct(time))
+fps <- 10 
+duration <- nframes / fps  
+animate(animation, renderer = gifski_renderer("deplacement.gif"), width = 2000, height = 1333, duration = duration)
