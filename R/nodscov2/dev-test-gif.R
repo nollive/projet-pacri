168fc4e5fb5a28b7244977fcca0dac5c4ba9d414
nollive <olivier.gaufres@gmail.com>
2024-06-28 17:39:36 +0200

    changed the size of texts for better visibility

--- R/nodscov2/dev-test-gif.R
+++ R/nodscov2/dev-test-gif.R
@@ -5,20 +5,9 @@ library(dplyr)
 library(ggrepel)
 library(tidyr)
 
-
-
-#loc_path <- file.path("dev-localization-nodscov2.RData")
 wd <- getwd()
-loc_path <- file.path(wd, "..", "..", "out", "loc-nodscov2","dev-localization-nodscov2.RData")
+loc_path <- file.path(wd, "out", "loc-nodscov2","dev-localization-nodscov2.RData")
 load(loc_path)
-
-
-new_begin_date <- begin_date + (5*60*60) ## OFFSET TO START THE DAY AT 12AM
-new_end_date <- new_begin_date + (24*60*60)
-print(new_end_date - new_begin_date)
-t_begin <- 5*60*2 ## OFFSET t
-t_end <- (t_begin + 24*60*2) - 1
-
 
 distribute_points <- function(n) {
   theta <- seq(0, 2 * pi, length.out = n + 1)[-1]
@@ -53,9 +42,15 @@ rooms_coords <- rooms %>%
   distinct(id_room, .keep_all = TRUE) %>%
   select(localization, x, y)
 
+new_begin_date <- begin_date + (5*60*60) ## OFFSET TO START THE DAY AT 12AM
+new_end_date <- new_begin_date + (24*60*60)
+print(new_end_date - new_begin_date)
+t_begin <- 5*60*2 ## OFFSET t
+t_end <- (t_begin + 24*60*2) - 1
 
+
+#data <- do.call(rbind, global_localization)
 data <- do.call(rbind, global_localization) %>% filter(between(time, t_begin, t_end))
-data <- do.call(rbind, global_localization) %>% filter(time < 50)
 data <- data %>%
   filter(localization != -1) %>%
   left_join(rooms_coords, by = c("localization")) %>%
@@ -100,25 +95,24 @@ segments_data <- data %>%
 
 
 # Plot
-p <- ggplot(data %>% filter(time > 10), aes(x = x_adj, y = y_adj, color = cat, group = id)) +
+p <- ggplot(data, aes(x = x_adj, y = y_adj, color = cat, group = id)) +
   geom_point(size = 5) +
   #geom_segment(data = segments_data, aes(xend = x_end, yend = y_end), alpha = 0.5) +
-  geom_text_repel(aes(label = id), size = 3, box.padding = 0.5, point.padding = 0.5, max.overlaps = Inf) +
+  geom_text_repel(aes(label = id), size = 5, box.padding = 0.5, point.padding = 0.5, max.overlaps = Inf) +
   scale_color_discrete(name = "Category") +
   scale_y_continuous(breaks = 1:4, labels = c("Patient room", "Corridor", "Office \n Nursing station", "Restroom")) +
-  scale_x_continuous(breaks = 1:(sum(rooms_coords$y == 1)+1), labels = c(paste0("Chambre ", 1:(sum(rooms_coords$y == 1)+1)))) +
-  labs(title = 'Individual position at time : {begin_date + (frame_time * 30)}, at time step : {frame_time}', x = 'Patient room', y = 'Type of room') +
-  theme_minimal() + 
+  scale_x_continuous(breaks = 1:(sum(rooms_coords$y == 1)+1), labels = c(paste0("Room ", 1:(sum(rooms_coords$y == 1)+1)))) +
+  labs(title = 'Individual position at time : {begin_date + (frame_time * 30)},  at time step : {frame_time}', x = 'Patient room', y = 'Type of room') +
+  theme_minimal() +
   theme(
-    plot.title = element_text(size = 22, hjust = 0.5), # 
-    axis.text = element_text(size = 18), 
-    axis.title = element_text(size = 18), 
-    legend.text = element_text(size = 20),
-    legend.title = element_text(size = 22) 
+    plot.title = element_text(size = 45, hjust = 0.5), #
+    axis.text = element_text(size = 30),
+    axis.title = element_text(size = 40),
+    legend.text = element_text(size = 35),
+    legend.title = element_text(size = 40)
   )+
   coord_cartesian(clip = "off")
 
-#print(p)
 animation <- p +
   transition_time(time) +
   ease_aes('linear')
@@ -127,5 +121,4 @@ animation <- p +
 nframes <- nrow(data%>%distinct(time))
 fps <- 50
 duration <- nframes / fps
-animate(animation, renderer = gifski_renderer("deplacement.gif"), width = 2000, height = 1333, fps = 50, nframes = nframes, duration = duration)
-
+animate(animation, renderer = gifski_renderer("trajectories-gif.gif"), width = 3000, height = 1333, fps = 50, nframes = nframes, duration = duration)
