f9387ac5899a987a0cdd6857844909a138175935
mlayan <m.layan@hotmail.fr>
2024-10-09 16:51:42 +0200

    Update input data for synthetic network algorithm + Add comparison of real and synthetic networks

---
+++ R/nodscov2/helper-functions.R
@@ -0,0 +1,130 @@
+################################################################################
+##          Support functions, dictionaries and variables
+##                   to analyze interaction data
+################################################################################
+
+# Dictionaries------------------------------------------------------------------
+dict_cat = c("aux nurse" = "Paramedical", 
+             "nurse" = "Paramedical",
+             "student nurse" = "Paramedical",
+             "reeducation staff" = "Paramedical",
+             "ext physician" = "Medical",
+             "physician" = "Medical")
+
+# Plots-------------------------------------------------------------------------
+pal = c("Observed" = "darkorchid", "Reconstructed"= "darkorange")
+
+# Variables---------------------------------------------------------------------
+noon_day1 = as_datetime("2020-05-06 12:00:00")
+noon_day2 = as_datetime("2020-05-07 12:00:00")
+noon_last_day = noon_day1 + 90*3600*24
+
+# Functions---------------------------------------------------------------------
+# Get vector of hours between two POSIXct dates 
+unroll_dates = function(df) {
+  if ("DATEREMISE" %in% colnames(df)) {
+    out = data.frame(
+      date_list=seq(df$DATEREMISE, df$DATEREC, 3600)
+    )
+  }
+  
+  if ("firstDate" %in% colnames(df)) {
+    out = seq(floor_date(as_datetime(df$firstDate), "hour"), floor_date(as_datetime(df$lastDate), "hour"), 3600)
+  }
+  
+  return(out)
+}
+
+# Expand.grid for dataframes (combine all rows of two distinct dataframes)
+expand.grid.df <- function(...) Reduce(function(...) merge(..., by=NULL), list(...))
+
+# Function to return a set of network metrics (from Leclerc et al., 2024)
+# https://gitlab.pasteur.fr/qleclerc/network_algorithm/-/tree/main?ref_type=heads
+get_net_metrics = function(graph_data, adm_data, iter = 0, network = "Observed"){
+  
+  days = unique(graph_data$date_posix)
+  
+  data = data.frame(degrees = rep(0, length(days)),
+                    densities = rep(0, length(days)),
+                    transitivities = rep(0, length(days)),
+                    assortativities = rep(0, length(days)),
+                    efficiencies = rep(0, length(days)),
+                    temp_corr = rep(0, length(days)),
+                    iter = iter,
+                    network = network,
+                    day = days)
+  
+  
+  for(i in 1:length(days)){
+    
+    # full network
+    data_d = graph_data %>%
+      filter(date_posix == days[i])
+    
+    graph_d = graph_from_data_frame(data_d, directed = F)
+    graph_d = simplify(graph_d)
+    vertex_atts = data.frame(id = vertex_attr(graph_d, "name")) %>%
+      left_join(adm_data, "id")
+    graph_d = graph_d %>%
+      set_vertex_attr("cat", value = vertex_atts$cat) %>%
+      set_vertex_attr("cat_ag", value = vertex_atts$cat_ag) %>%
+      set_vertex_attr("staff", value = vertex_atts$staff) %>%
+      set_vertex_attr("ward", value = vertex_atts$ward)
+    
+    data$degrees[i] = mean(degree(graph_d))
+    data$densities[i] = edge_density(graph_d)
+    data$transitivities[i] = transitivity(graph_d)
+    data$assortativities[i] = assortativity_degree(graph_d, directed = F)
+    data$efficiencies[i] = global_efficiency(graph_d, directed = F)
+    
+  }
+  
+  if (length(days) > 1) {
+    data$temp_corr = c(0, temporal_correlation(graph_data)) 
+  }
+  
+  return(data)
+  
+}
+
+# Function to calculate the temporal correlation of a network (from Leclerc et al., 2024)
+# https://gitlab.pasteur.fr/qleclerc/network_algorithm/-/tree/main?ref_type=heads
+temporal_correlation = function(graph_data){
+  
+  temp_cor = c()
+  
+  for(i in 1:(length(unique(graph_data$date_posix))-1)){
+    
+    # full network
+    data_t1 = graph_data %>%
+      filter(date_posix == unique(graph_data$date_posix)[i])
+    graph_t1 = graph_from_data_frame(data_t1, directed = F)
+    graph_t1 = simplify(graph_t1)
+    
+    data_t2 = graph_data %>%
+      filter(date_posix == unique(graph_data$date_posix)[i+1])
+    graph_t2 = graph_from_data_frame(data_t2, directed = F)
+    graph_t2 = simplify(graph_t2)
+    
+    temp_cor_indiv = c()
+    
+    for(indiv in names(V(graph_t1))){
+      if(indiv %in% names(V(graph_t2))){
+        
+        neighbours_t1 = names(neighbors(graph_t1, indiv))
+        neighbours_t2 = names(neighbors(graph_t2, indiv))
+        temp_cor_indiv = c(temp_cor_indiv,
+                           length(intersect(neighbours_t1, neighbours_t2))/sqrt(length(neighbours_t1)*length(neighbours_t2)))
+        
+      } else temp_cor_indiv = c(temp_cor_indiv,0)
+      
+    }
+    
+    temp_cor = c(temp_cor, mean(temp_cor_indiv))
+  }
+  
+  return(temp_cor)
+}
+
+
+
